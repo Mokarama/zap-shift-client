1f907ebc30b8c49ded93166623ec3e1e63a4dc39
Mokarama <mokaramaaktershanta@gmail.com>
2025-10-15 14:30:26 +0600

    Fetch parcels from backend and display in MyParcels component

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 
 
 const MyParcels = () => {
+    const [parcels, setParcels]=useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/parcels')
+        .then(res=>res.json())
+        .then(data=>setParcels(data))
+
+        .catch(err=>console.log("Error fetching parcels",err))
+
+    },[])
     return (
         <div>
            <h2>My Parcels coming here</h2> 
+           {
+            parcels.length===0 ?(
+                <p>No parcels found</p>
+            ):(
+                <ul>
+                    {
+                     parcels.map(parcel=>(
+                        <li>
+                            {parcel.sender_name}-{parcel.receiver_name}
+                        </li>
+                     ))
+                    }
+                </ul>
+            )
+           }
         </div>
     );
 };
